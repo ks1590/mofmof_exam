943c69c6bb109fe31e0f2912f77af6c9faa9f4f7
kensuke suzuki <35k.0101@gmail.com>
2020-11-21 12:06:55 +0900

    add:最寄駅のdb

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 2020_11_18_122543) do
+ActiveRecord::Schema.define(version: 2020_11_21_025844) do
 
   create_table "rentals", force: :cascade do |t|
     t.string "name"
@@ -7,6 +7,17 @@ ActiveRecord::Schema.define(version: 2020_11_18_122543) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "note"
+  end
+
+  create_table "stations", force: :cascade do |t|
+    t.string "route"
+    t.string "name"
+    t.integer "distance"
+    t.integer "rental_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rental_id"], name: "index_stations_on_rental_id"
   end
 
 end
